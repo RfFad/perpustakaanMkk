79a30998d92714bf2530c5f85d97352c3210032d
= <=>
2025-01-27 19:46:43 -1200

    login

--- auth/login.php
+++ auth/login.php
@@ -1,87 +1,113 @@
 <?php 
 include '../config.php';
+session_start();
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Query untuk memeriksa user
+    $query = "SELECT * FROM users WHERE email = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+
+        // Verifikasi password
+        if (password_verify($password, $user['password'])) {
+            // Simpan data user ke session
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['role'] = $user['role'];
+
+            // Redirect berdasarkan role
+            if ($user['role'] === 'Admin') {
+                header("Location: admin/dashboard.php");
+            } elseif ($user['role'] === 'Operator') {
+                header("Location: operator/dashboard.php");
+            } else {
+                echo "Role tidak dikenal.";
+            }
+            exit();
+        } else {
+            $error = "Password salah.";
+        }
+    } else {
+        $error = "Email tidak ditemukan.";
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Login</title>
+    <title>Login</title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= BASE_URL ?>/public/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="<?= BASE_URL ?>/public/css/sb-admin-2.min.css" rel="stylesheet">
     <style>
-      .bg-login-image {
-    background-image: url('<?= BASE_URL ?>/asset/background/perpustakaan.jpg');
-    background-size: cover; /* Gambar memenuhi div */
-    background-position: center; /* Posisikan gambar di tengah */
-    background-repeat: no-repeat; /* Hindari pengulangan gambar */
-    height: 50%; /* Pastikan tinggi div sesuai */
-    min-height: 50vh; /* Tambahkan minimal tinggi untuk viewport penuh */
-}
-
+        .bg-login-image {
+            background-image: url('<?= BASE_URL ?>/asset/background/perpustakaan.jpg');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            height: 50%;
+            min-height: 50vh;
+        }
     </style>
 </head>
 
 <body class="bg-gradient-primary">
-
     <div class="container">
-
         <!-- Outer Row -->
         <div class="row justify-content-center">
-
             <div class="col-xl-10 col-lg-12 col-md-9">
-
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image" >
-                              
-                            </div>
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
-                                    <form class="user">
+                                    <?php if (isset($error)): ?>
+                                        <div class="alert alert-danger" role="alert">
+                                            <?= $error ?>
+                                        </div>
+                                    <?php endif; ?>
+                                    <form class="user" method="POST" action="">
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                            <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." required>
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password">
+                                            <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" required>
                                         </div>
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
                                             Login
-                                        </a>
+                                        </button>
                                     </form>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
-
     </div>
 
     <!-- Bootstrap core JavaScript-->
@@ -89,11 +115,10 @@ include '../config.php';
     <script src="<?= BASE_URL ?>/public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="<?= BASE_URL ?>/publicvendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?= BASE_URL ?>/public/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
     <script src="<?= BASE_URL ?>/public/js/sb-admin-2.min.js"></script>
-
 </body>
 
 </html>
--- config.php
+++ config.php
@@ -1,4 +1,16 @@
 <?php
 define('BASE_URL', 'http://localhost/perpustakaanMkk');
 
+// Koneksi ke database
+$host = 'localhost';    // Host database
+$user = 'root';         // Username database
+$pass = '';             // Password database (kosong untuk XAMPP)
+$db   = 'wb_perpus';    // Nama database Anda
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Periksa koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
 ?>
